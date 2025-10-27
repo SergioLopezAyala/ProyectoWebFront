5c99477b8973d3fe676d4caf1886c647a4a89d33
sl345248 <sl39718@gmail.com>
2025-10-27 08:59:19 -0500

    organizacion componentes y creacion de componentes faltantes

---
+++ src/app/gestion-de-roles-de-proceso/editar-rol-de-proceso/editar-rol-de-proceso.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-rol-de-proceso',
+  imports: [],
+  templateUrl: './editar-rol-de-proceso.html',
+  styleUrl: './editar-rol-de-proceso.css'
+})
+export class EditarRolDeProceso {
+
+}
---
+++ src/app/gestion-de-roles-de-proceso/eliminar-rol-de-proceso/eliminar-rol-de-proceso.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-eliminar-rol-de-proceso',
+  imports: [],
+  templateUrl: './eliminar-rol-de-proceso.html',
+  styleUrl: './eliminar-rol-de-proceso.css'
+})
+export class EliminarRolDeProceso {
+
+}
